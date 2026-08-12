8855ff50e49b835c2757cd2f3cdc43641112ed92
emelinsuju <emelinsuju@gmail.com>
2026-08-12 21:27:45 +0530

    Add files via upload

---
+++ tutorial6qn1.java
@@ -0,0 +1,18 @@
+class Car {
+    private String model;
+
+    public Car(String model) {
+        this.model = model;
+    }
+
+    public void showModel() {
+        System.out.println("Model: " + model);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car("Tesla Model S");
+        myCar.showModel();
+    }
+}
